1dd9713f48d64c9831da31d3860341e1e589e531
bngure <brianwahinyangure@gmail.com>
2024-04-28 21:19:02 +0300

    HotFix: Icon Replay adjusted

--- src/components/Icon/Icon.jsx
+++ src/components/Icon/Icon.jsx
@@ -1,24 +1,10 @@
-import { MdOutlineDeleteSweep } from "react-icons/md";
-import { VscColorMode } from "react-icons/vsc";
-import { MdDarkMode } from "react-icons/md";
-import { MdOutlineDarkMode } from "react-icons/md";
-import { MdLightMode } from "react-icons/md";
-import { FcAbout } from "react-icons/fc";
-import { BsFillInfoCircleFill } from "react-icons/bs";
-import { TfiBarChartAlt } from "react-icons/tfi";
 import { IoPlayCircleOutline } from "react-icons/io5";
-import { MdOutlineReplayCircleFilled } from "react-icons/md";
 import { FaRegCirclePause } from "react-icons/fa6";
+import { MdReplay } from "react-icons/md";
 
 const iconsObj = {
-  light: <MdLightMode className="light" />,
-  dark: <MdDarkMode />,
-  clear: <MdOutlineDeleteSweep />,
-  //   about: <BsFillInfoCircleFill />,
-  about: <FcAbout />,
-  chart: <TfiBarChartAlt />,
   play: <IoPlayCircleOutline />,
-  restart: <MdOutlineReplayCircleFilled />,
+  restart: <MdReplay />,
   pause: <FaRegCirclePause />,
 };
 
